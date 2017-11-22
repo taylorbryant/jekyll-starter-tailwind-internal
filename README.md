15dc88c3638132fcd5af3da61972b354cac62ef6
Taylor Bryant <taylor@bryant.io>
2017-11-21 21:26:28 -0600

    Upload v1

---
+++ README.md
@@ -0,0 +1,31 @@
+# Tailwind Jekyll Starter Kit
+A starter kit for using [Tailwind](https://tailwindcss.com) (v0.2.2) with [Jekyll](https://jekyllrb.com/) that includes:
+* A barebones Jekyll starter theme
+* A Gulp file with a default task that does the following:
+
+    * Builds Tailwind
+    * Runs [Autoprefixer](https://github.com/postcss/autoprefixer)
+    * Minifies your CSS
+    * Builds Jekyll
+    * Runs [Browsersync](https://www.browsersync.io/) for live reload
+
+## What is Tailwind?
+>"Tailwind is a utility-first CSS framework for rapidly building custom user interfaces."
+– [Tailwind](https://tailwindcss.com)
+
+## What is Jekyll?
+>"Jekyll is a simple, blog-aware, static site generator perfect for personal, project, or organization sites. Think of it like a file-based CMS, without all the complexity. Jekyll takes your content, renders Markdown and Liquid templates, and spits out a complete, static website ready to be served by Apache, Nginx or another web server. Jekyll is the engine behind GitHub Pages, which you can use to host sites right from your GitHub repositories."
+– [Jekyll](https://jekyllrb.com/)
+
+## Requirements 
+* [Bundler](http://bundler.io/)
+* [gulp-cli](https://www.npmjs.com/package/gulp-cli)
+* [Jekyll](https://jekyllrb.com/)
+* [Node.js](https://nodejs.org/en/)
+* [npm](https://www.npmjs.com/)
+* [Ruby](https://www.ruby-lang.org/en/)
+
+## Get started
+* `bundle install` to install Ruby gems
+* `npm install` to install npm packages
+* `gulp` to run the default Gulp task
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const child = require('child_process');
+const browserSync = require('browser-sync').create();
+
+const siteRoot = '_site';
+const mainCSS = 'src/style.css'; /* Main stylesheet (pre-build) */
+const tailwindConfig = 'tailwind.js'; /* Tailwind config */
+
+const jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll'; /* Fix Windows compatibility issue */
+
+/**
+ * Build Jekyll Site
+ */
+gulp.task('jekyll-build', ['css'], function () {
+  browserSync.notify('Running: $ jekyll build');
+  return child.spawn(jekyll, ['build'], { stdio: 'inherit' });
+});
+
+/**
+ * Compile styles
+ */
+gulp.task('css', function () {
+  const atimport = require('postcss-import');
+  const postcss = require('gulp-postcss');
+  const tailwindcss = require('tailwindcss');
+  const autoprefixer = require('gulp-autoprefixer');
+  const cleancss = require('gulp-clean-css');
+
+  return gulp.src(mainCSS)
+    .pipe(postcss([
+      atimport(),
+      tailwindcss(tailwindConfig)
+    ]))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleancss())
+    .pipe(gulp.dest('_includes/'))
+});
+
+/**
+ * Serve site with BrowserSync
+ */
+gulp.task('serve', ['jekyll-build'], () => {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 4000,
+    open: "local",
+    server: {
+      baseDir: siteRoot
+    }
+  });
+
+  gulp.watch([mainCSS, tailwindConfig, '**/*.html', '**/*.md', '**/*.yml', '!_site/**/*'], ['jekyll-build']);
+});
+
+gulp.task('default', ['serve']);
